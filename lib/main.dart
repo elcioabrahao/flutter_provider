2334f7bb87b23d9337279853a1dc6ced5517cd0a
Elcio Abrahao <elcioabrahao@usp.br>
2019-10-25 15:26:26 -0200

    Initial commit, tela principal com um List View que acessa uma API via http e retorna uma lista que é renderizada na tela em um scrollview. A classe App_State controla o estago geral da aplicacão.

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/response_display.dart';
+import 'package:flutter_provider/text_display.dart';
+import 'package:flutter_provider/text_edit_widget.dart';
+import 'package:provider/provider.dart';
+import 'app_state.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider<AppState>(
+          builder: (_) => AppState(),
+          child: MyHomePage(),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context);
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                TextDisplay(),
+                TextEditWidget(),
+                RaisedButton(
+                  onPressed: () => appState.fetchData(),
+                  child: Text("Fetch Data from Network"),
+                ),
+                ResponseDisplay(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
